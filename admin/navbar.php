eef23ca4fb9e7679c1894750d0bf10ae71b80b0a
keitaabdou <abdou.keita@uvci.edu.ci>
2021-04-08 10:09:06 +0000

    affichage de la navbar lorque l'admin est connecter

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,15 +1,18 @@
+<?php session_start(); ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#"><img src="../public/assets/images/logo-igs2.png" alt="" class="logo-igs"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+<?php   if(isset($_SESSION["id"])){
+ ?>
+<div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav ml-auto">
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 Paramètres        </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#"><?php echo $_SESSION["nom"]." ". $_SESSION["prenoms"]?></a>
+          <a class="dropdown-item" href="#"><?php if(isset($_SESSION["id"])){echo $_SESSION["nom"]." ". $_SESSION["prenoms"];}?></a>
           <a class="dropdown-item" href="logout.php">Déconnexion</a>
         </div>
       </li>
@@ -26,4 +29,9 @@ Apprenants        </a>
       </li>
     </ul>
   </div>
+ <?php  
+}
+
+?>
+  
 </nav>
