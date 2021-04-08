cc9f889e7698973d1c80aae6fa77b26b2e367b43
keitaabdou <abdou.keita@uvci.edu.ci>
2021-04-08 09:54:36 +0000

    suppression des fichiers avec erreur

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#"><img src="../public/assets/images/logo-igs2.png" alt="" class="logo-igs"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,34 +26,4 @@ Apprenants        </a>
       </li>
     </ul>
   </div>
-=======
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img src="../public/assets/images/logo-igs2.png" alt="" class="logo-igs"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav ml-auto">
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-Paramètres        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#"><?php echo $_SESSION["nom"]." ". $_SESSION["prenoms"]?></a>
-          <a class="dropdown-item" href="logout.php">Déconnexion</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-Apprenants        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Ajouter</a>
-          <a class="dropdown-item" href="liste-presence.php">Liste</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Presences</a>
-      </li>
-    </ul>
-  </div>
->>>>>>> 8a07b457281546fe3510126cebf022e5558d636b
 </nav>
